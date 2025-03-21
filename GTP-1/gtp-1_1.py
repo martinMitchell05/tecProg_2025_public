f2ec2b7e8c80390972e867e7c24c907098d215c5
Mitchell Martín <martinmitchellmartin95@gmail.com>
2025-03-20 23:35:13 -0300

    Primer commit del repositorio de GTP

---
+++ GTP-1/gtp-1_1.py
@@ -0,0 +1,55 @@
+### ejercicio 1: Calculadora de Edad
+
+#nacimiento = int(input("Introduce tu año de nacimiento: "))
+#actual = int(input("Introduce el año actual: "))
+
+#edad = actual - nacimiento
+
+#print("Tu edad es: ", edad)
+
+### ejercicio 2: Conversión de segundos
+
+#segundos = int(input("Introduce el numero en segundos: "))
+
+#hora = int(segundos/3600)
+#minutos = int((segundos%3600)/60)
+#segundos =  int(segundos%60)
+
+#print("La hora es:", hora, "hs", minutos, "min", segundos, "seg")
+
+### ejercicio 3: Calculadora de Raices Cuadradas
+
+#import math
+
+#numero = float(input("Introduce un numero: "))
+
+#while (numero > 0):
+
+    #raiz = math.sqrt(numero)
+
+    #print("Tu raiz es: ", raiz)
+
+    #numero = float(input("Introduce un numero: "))
+
+#print("La raiz no existe")
+
+
+### ejercicio 4: Calculo del IMC
+
+#peso = float(input("Introduce tu peso en kg: "))
+#altura = float(input("Introduce tu altura en metros: "))
+
+#imc = peso/(altura*altura)
+
+#if imc>=40:
+    #print("Obesidad tipo III")
+#elif imc>=30:
+    #print("Obesidad")
+#elif imc>=25:
+    #print("Sobrepeso")
+#elif imc>=18.5:
+    #print("Normal")
+#else:
+    #print("Insuficiente")
+
+
